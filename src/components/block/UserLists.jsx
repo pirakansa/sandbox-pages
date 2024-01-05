ee9cf1252538c091fdf1086762e0bf799fe8f381
pirakansa <pirakansa@users.noreply.github.com>
2024-01-05 14:33:33 +0900

    fix list warning

--- src/components/block/UserLists.jsx
+++ src/components/block/UserLists.jsx
@@ -18,21 +18,19 @@ function UserListsContent({ users }) {
 
   const generate = users.map((value, idx) => {
     return (
-      <>
-        <ListItem alignItems="flex-start" divider={true} key={idx} >
-          <ListItemAvatar>
-            <Avatar alt={value.id} src={value.avatar_url} />
-          </ListItemAvatar>
-          <ListItemText
-            primary={value.login}
-            secondary={
-              <a href={value.html_url} target="_blank" rel="noreferrer" >
-                  {value.html_url}
-              </a>
-            }
-          />
-        </ListItem>
-      </>
+      <ListItem alignItems="flex-start" divider={true} key={idx} >
+        <ListItemAvatar>
+          <Avatar alt={value.login} src={value.avatar_url} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={value.login}
+          secondary={
+            <a href={value.html_url} target="_blank" rel="noreferrer" >
+                {value.html_url}
+            </a>
+          }
+        />
+      </ListItem>
     );
   });
 
